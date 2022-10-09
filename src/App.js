f5a4823b771248268b1c7730ff81ce6aeae5ae8f
tasnim63 <tamanna202163@gmail.com>
2022-10-09 15:16:58 +0100

    bg added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.scss'
+function App() {
+  return (
+    <>
+    <h1>hello world</h1>
+    <div className="sidebar"></div>
+    <div className="main-content"></div>
+    </>
+  );
+}
+
+export default App;
