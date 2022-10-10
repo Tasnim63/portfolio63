cfbfa1b30a6cc68a5e75f9ae815844c1943350e2
tasnim63 <tamanna202163@gmail.com>
2022-10-10 12:36:14 +0100

    icon styled

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+   <>
+   <div className="AboutPage">
+    
+   </div>
+   </>
+  )
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,26 @@
 import React from "react";
-import { AiFillGithub,AiFillLinkedin,AiFillFacebook } from 'react-icons/ai';
-import { Link } from "react-router-dom";
+import { AiFillGithub, AiFillLinkedin, AiFillFacebook } from "react-icons/ai";
+
 export default function HomePage() {
   return (
     <>
       <div className="HomePage">
         <header className="hero">
           <h1 className="hero_text">
-          Hi , I am
+            Hi , I am
             <span> Tamanna</span>
           </h1>
           <h6>Junior React js Developer</h6>
           <div className="icons">
-            <a href="https://github.com/Tasnim63"><AiFillGithub></AiFillGithub></a>
-      <span></span>
-      <span><AiFillLinkedin></AiFillLinkedin></span>
-      <span><AiFillFacebook></AiFillFacebook></span>
+            <a href="https://github.com/Tasnim63" className="icon-holder">
+              <AiFillGithub className="icon gh"></AiFillGithub>
+            </a>
+            <a href="https://www.linkedin.com/in/tamanna-akter-95101a228/" className="icon-holder">
+              <AiFillLinkedin className="icon li"></AiFillLinkedin>
+            </a>
+            <a href="https://github.com/Tasnim63" className="icon-holder">
+              <AiFillFacebook className="icon fb"></AiFillFacebook>
+            </a>
           </div>
         </header>
       </div>
