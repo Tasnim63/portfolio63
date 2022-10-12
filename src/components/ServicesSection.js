812a5208ba3cbf8f32ad7a71697aaff47367f961
tasnim63 <tamanna202163@gmail.com>
2022-10-12 13:54:38 +0100

    card title added

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -6,7 +6,7 @@ export default function ServicesSection({image}) {
         <div className="service">
         <img src={image} alt="" />
         <h5 className="s-title">
-
+           
         </h5>
         <p className="s-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ad, officia, aliquam sunt laborum, fuga autem officiis suscipit earum quo voluptates reprehenderit cum eius temporibus voluptatibus. Rerum harum neque odit.
