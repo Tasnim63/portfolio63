1d37907158fd0b90cd12278cd4d7b43e96d53c79
tasnim63 <tamanna202163@gmail.com>
2022-10-12 10:48:14 +0100

    all shill added

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,6 @@ import Skill from "../components/Skill";
 
 import Tittle from "../components/Tittle";
 
-
 export default function AboutPage() {
   return (
     <>
@@ -12,17 +11,17 @@ export default function AboutPage() {
         <Tittle title={"About Me"} span={"About Me"} />
         <ImageSection />
         <Tittle title={"My Skills"} span={"My Skills"} />
-     <div className="skills-container">
-     <Skill skill={'Html'} progress={'80%'} width={'80%'} />
-     <Skill skill={'Css'} progress={'80%'} width={'80%'} />
-     <Skill skill={'Taiwind'} progress={'80%'} width={'80%'} />
-     <Skill skill={'Boottrap 5'} progress={'80%'} width={'80%'} />
-     <Skill skill={'javascript'} progress={'70%'} width={'70%'} />
-     <Skill skill={'React js'} progress={'70%'} width={'70%'} />
-     <Skill skill={'Node js'} progress={'30%'} width={'30%'} />
-     <Skill skill={'MongoDb'} progress={'70%'} width={'70%'} />
-     <Skill skill={'Express js'} progress={'30%'} width={'30%'} />
-     </div>
+        <div className="skillsContainer">
+          <Skill skill={"Html"} progress={"80%"} width={"80%"} />
+          <Skill skill={"Css"} progress={"80%"} width={"80%"} />
+          <Skill skill={"Taiwind"} progress={"80%"} width={"80%"} />
+          <Skill skill={"Boottrap 5"} progress={"80%"} width={"80%"} />
+          <Skill skill={"javascript"} progress={"70%"} width={"70%"} />
+          <Skill skill={"React js"} progress={"70%"} width={"70%"} />
+          <Skill skill={"Node js"} progress={"30%"} width={"30%"} />
+          <Skill skill={"MongoDb"} progress={"30%"} width={"30%"} />
+          <Skill skill={"Express js"} progress={"30%"} width={"30%"} />
+        </div>
       </div>
     </>
   );
