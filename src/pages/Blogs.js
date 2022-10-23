b69c721d28cb8ef5ef29d0c5903914f7e43d6565
tasnim63 <tamanna202163@gmail.com>
2022-10-23 04:06:53 +0100

    blogs titl eadded

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import allBlogs from '../components/allBlogs'
+import Tittle from '../components/Tittle'
 export default function Blogs() {
   return (
     <div className='BlogsPage'
     >
+      <Tittle title={'My Blogs'} span={'My Blogs'}></Tittle>
       {
         allBlogs.map((blog)=>{
           return <div className="blog" key={blog.id}>
