20704d4581e38cb22376d58efad464aea4bec9af
tasnim63 <tamanna202163@gmail.com>
2022-10-23 08:27:13 +0100

    map added

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-export default function ContactItem() {
+export default function ContactItem({icon,text1,text2,title}) {
   return (
-    <div>ContactItem</div>
+    <div className='ContactItem'>
+      <div className="contact">
+        <img src={icon} alt="" />
+        <div className="right-item">
+          <h5>{title}</h5>
+          <p>{text1}</p>
+          <p>{text2}</p>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import ContactItem from '../components/ContactItem'
-
+import phone from '../images/phone.svg';
+import email from '../images/emailme.svg';
+import location from '../images/location.svg';
 export default function Contact() {
   return (
     <div className='ContactPage'>
       <div className="map-sect">
-        <div className="contact-sect">
-          <ContactItem/>
-        </div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20693.24360958173!2d91.02009991484636!3d23.529509052046347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37546519785fe84f%3A0x7c58e5f6bb99999b!2sBiharmandol%2C%20Mohanpur!5e0!3m2!1sen!2sbd!4v1666509718121!5m2!1sen!2sbd" width="600" height="450" style={{border:0}} allowFullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+        
       </div>
+      <div className="contact-sect">
+          <ContactItem icon={phone} text1={'+8801.......'}  title={'phone'}/>
+          <ContactItem icon={email} text1={'tamanna202163@gmail.com'}  title={'Email'}/>
+          <ContactItem icon={location} text1={'comilla , Bangladesh'}  title={'Address'}/>
+        </div>
     </div>
   )
 }
